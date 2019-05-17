934c0b41da26f3703724a6e3458bdf5fa67eb495
mallikar <mallikar@IN-7SMH5H2.mshome.net>
2019-05-17 09:26:53 +0530

    FirstCommit

---
+++ Selenium1/src/selenium1/DoubleClick.java
@@ -0,0 +1,35 @@
+package selenium1;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class DoubleClick 
+{
+	static
+	{
+		System.setProperty("webdriver.chrome.driver","./Drivers/chromedriver.exe");
+		System.setProperty("webdriver.gecko.driver","./Drivers/geckodriver.exe");
+	}
+	
+	public static void main(String[] args) throws InterruptedException, IOException 
+	{
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("file:///C:/Users/mallikar/Desktop/Example6.html");
+		WebElement ele = driver.findElement(By.id("dblClkBtn"));
+		Actions action = new Actions(driver);
+		action.doubleClick(ele).perform();
+		Thread.sleep(2000);
+		driver.switchTo().alert().accept();
+		
+	
+	}
+}
---
+++ Selenium1/src/selenium1/RightClick.java
@@ -0,0 +1,41 @@
+package selenium1;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class RightClick 
+{
+	static
+	{
+		System.setProperty("webdriver.chrome.driver","./Drivers/chromedriver.exe");
+		System.setProperty("webdriver.gecko.driver","./Drivers/geckodriver.exe");
+	}
+	
+	public static void main(String[] args) throws InterruptedException, IOException, AWTException 
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://demo.actitime.com/login.do");
+		WebElement ele = driver.findElement(By.xpath("//a[text()='actiTIME Inc.']"));
+		Actions action = new Actions(driver);
+		action.contextClick(ele);
+		Robot r = new Robot();
+		r.keyPress(KeyEvent.VK_T);
+		r.keyRelease(KeyEvent.VK_T);
+		
+		
+	
+	}
+}
